7cb84c7a96ded82e71afe09b1da61effa1698e05
fred <zhangwenhi@sina.com>
2015-08-28 18:36:00 +0800

    finish several insects,topic model and a python study demo

--- webInsect/TEDweb.py
+++ webInsect/TEDweb.py
@@ -6,11 +6,17 @@ from bs4 import *
 from bs4 import Tag
 import bs4
 import codecs
+import gevent
+from gevent import monkey
+from gevent.queue import Queue, Empty
+monkey.patch_all()
 
 
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
+
+tasks = Queue(maxsize=36)#one page 36 url
 
 def get_Data(language,link):
     list=[]
@@ -43,38 +49,86 @@ def get_article_links(num):
         href_list.append(str(anchor['href']))
     return href_list
 
+
+
 def saveToFile(filePath,list=[]):
     afile=open(filePath,'w')
     for line in list:
             afile.write(line+"\n")
     afile.close()
 
-num = 30
-foldernum=1043
-full_links = [];
-while True:
-    print num
-    num+=1
-    links = get_article_links(num)
-    if(links and len(links)==0):
-        print "no more links"
-        break
-    else:
-        for link in links:
-            print(link)
-            if("michael_anti_behind_the_great_firewall_of_china" in link):
-                continue
-            filename = link['/talks/'.__len__():link.index('?')]
-            print filename
-            content=get_Data("zh-cn",link)
+
+
+def worker():
+    try:
+        foldernum=0
+        while True:
+            task = tasks.get()
+
+            filename = task['/talks/'.__len__():task.index('?')]
+            print "one woker get the"+filename+"task"
+            content=get_Data("zh-cn",task)
+            print "task "+filename+" got"
             if(not os.path.exists('D:/webdata/ted/'+filename)):
                 os.makedirs(r'D:/webdata/ted/'+filename)
             saveToFile(r'D:/webdata/ted/'+filename+'/zh-cn.txt',content)
-            content=get_Data("en",link)
+            content=get_Data("en",task)
             saveToFile(r'D:/webdata/ted/'+filename+'/en.txt',content)
             foldernum+=1
-            print str(foldernum)+' finished'
-print('finished!!!')
+            print str(foldernum)+" "+filename+' finished'
+
+    except Empty:
+        print "no task"
+
+def taskGenerator():
+    num=0
+    while True:
+        num=num+1
+        print num
+        links=get_article_links(num)
+        if(links and len(links)==0):
+            print "no more links"
+            break
+        else:
+            for link in links:
+                print(link)
+                tasks.put(link)
+workers=[gevent.spawn(worker) for i in range(0,4)]
+gevent.joinall([
+    gevent.spawn(taskGenerator),
+].append(workers))
+
+
+
+
+
+
+# num = 0
+# foldernum=0
+# full_links = [];
+# while True:
+#     print num
+#     num+=1
+#     links = get_article_links(num)
+#     if(links and len(links)==0):
+#         print "no more links"
+#         break
+#     else:
+#         for link in links:
+#             print(link)
+#             if("michael_anti_behind_the_great_firewall_of_china" in link):
+#                 continue
+#             filename = link['/talks/'.__len__():link.index('?')]
+#             print filename
+#             content=get_Data("zh-cn",link)
+#             if(not os.path.exists('D:/webdata/ted/'+filename)):
+#                 os.makedirs(r'D:/webdata/ted/'+filename)
+#             saveToFile(r'D:/webdata/ted/'+filename+'/zh-cn.txt',content)
+#             content=get_Data("en",link)
+#             saveToFile(r'D:/webdata/ted/'+filename+'/en.txt',content)
+#             foldernum+=1
+#             print str(foldernum)+' finished'
+# print('finished!!!')
 
 
 
